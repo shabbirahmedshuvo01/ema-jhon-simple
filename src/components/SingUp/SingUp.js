a647458c30915ed427f450796047d4f3df946646
Sabbir Shuvo <Shabbir Ahmed Shuvo>
2022-04-14 02:08:42 +0600

    from

---
+++ src/components/SingUp/SingUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SingUp.css';
+
+const SingUp = () => {
+    return (
+        <div>
+            <h2>SingUp</h2>
+        </div>
+    );
+};
+
+export default SingUp;
